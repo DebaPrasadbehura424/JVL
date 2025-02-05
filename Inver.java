63d906e46c3ecb3cb227b02b1b3222238102813c
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-02-05 12:01:26 +0530

    all my java questions

---
+++ Inver.java
@@ -0,0 +1,11 @@
+import java.util.*;
+//note java only supports 32 bit engine means 31 range must present other wise 0
+class Inver{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    Long n=sc.nextLong();
+    System.out.print(1.0/n);
+    
+
+   }   
+}
---
+++ RotateNumber.java
@@ -0,0 +1,64 @@
+import java.util.*;
+//note java only supports 32 bit engine means 31 range must present other wise 0
+class RotateNumber{
+  public static void main(String args[]){
+   Scanner sc= new Scanner(System.in);
+   int n =sc.nextInt();
+   int k=sc.nextInt();
+   ArrayList<Integer>arr= new ArrayList<>();
+   while(n>0){
+      int fact=n%10;
+      n=n/10;
+      arr.add(fact);
+   }
+
+
+   Collections.reverse(arr);
+   for(int num : arr){
+    System.out.print(num);
+   }
+   
+ k=k%arr.size()-1;
+
+
+//vowel check
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(c) != -1)
+                count++;
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
+
+
+//gcd
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 56, b = 98;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println("GCD is: " + a);
+    }
+}
+
+ 
+
+   
+   
+   
+
+   
+
+    
+
+   }   
+}

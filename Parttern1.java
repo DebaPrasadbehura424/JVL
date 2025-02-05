b9f5b073a8d97e8c19f87494cc712839c0045e68
DebaPrasadbehura424 <DebaPrasadbehura424@gmail.com>
2025-02-05 12:16:28 +0530

    x

--- Parttern1.java
+++ Parttern1.java
@@ -1,13 +1,13 @@
-class Parttern1{
-    public static void main(String args[]){
-     // for(int i=0;i<5;i++){
-        // for(int j=0;j<=i;j++){
-          // System.out.print("*");
-        // }
-        //  System.out.print("\n");
-
-     // }
-     //method 2:-  System.out.print("*\n**\n***\n****\n");
+class Parttern1 {
+  public static void main(String args[]) {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
 
     }
+    // method 2:- System.out.print("*\n**\n***\n****\n");
+
+  }
 }
